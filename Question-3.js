3a869b4d60de9634b21d1433f08e2c54d6cabf12
cezarqueiroz <cezarqueiroz18@gmail.com>
2024-10-03 15:49:19 -0400

    Question 3 finished

---
+++ Question-3.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+
+var logDirectory = './Logs';
+
+function removeFiles() {
+    if (fs.existsSync(logDirectory)) {
+        const files = fs.readdirSync(logDirectory);
+
+        for (let i = 0; i < files.length; i++) {
+            console.log('Delete files...', files[i]);
+            fs.unlinkSync(logDirectory + '/' +files[i]);
+        }
+        fs.rmdirSync(logDirectory);
+    } else {
+        console.log('Directory does not exist.');
+    }
+};
+
+function createFiles() {
+    if (!fs.existsSync(logDirectory)) {
+        fs.mkdirSync(logDirectory);
+    }
+    process.chdir(logDirectory);
+
+    for (let i = 0; i < 10; i++) {
+        const fileName = 'log' + i + '.txt';
+        fs.writeFileSync(fileName, 'Some text example' + i);
+        console.log(fileName);
+    }
+};
+
+removeFiles();
+createFiles();
